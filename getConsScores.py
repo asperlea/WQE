1673d761745c5ddb3645bcb09ccf58d33603e88f
Adriana <adriana@BC314BSRBJEAS.local>
2015-07-08 11:49:21 -0700

    Updated getConsScores.py

--- getConsScores.py
+++ getConsScores.py
@@ -18,18 +18,25 @@ AAGCTCTTCCAGAAGGTATGTGGAGAAGCACTGCTTTTCCTCTAGCTGGCCAAGGTCTAC
 '''
 
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: getConsScores.py <file1>")
-        #exit(1)
+    if len(sys.argv) < 3:
+        print("Usage: getConsScores.py <input file> <output file>")
+        exit(1)
+    exonsFile = open(sys.argv[1], "r")
+    consScores = open(sys.argv[2], "w")
 
     MamConserv = Wiggle(name='MamConserv',
                         wiggle_dir='/u/home/a/asperlea/project-ernst/phyloP45way',
                         file_prefix='chr',
                         file_suffix='.phyloP46way.placental.wigFix.gz')
 
-    print "Trying for chr10 region 100042558-100048772:\n"
-    regions = MamConserv.get_region('10', 100042558, 100048772)
-    print regions[0]
-    print regions[-1]
+    for line in exonsFile:
+        if line[0] == '>':
+            parsedLine = line.split("|")
+            chrom = parsedLine[1][3:]
+            chromStart = int(parsedLine[2])
+            chromEnd = int(parsedLine[3])
+            region = MamConserv.get_region(chrom, chromStart, chromEnd)
+
+            print region[0], region[1]
 
 main()
